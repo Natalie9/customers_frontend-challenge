503fd9ba084a5688f1924f037a9d5bc335a1f67f
natalie9 <natalietolentino9@gmail.com>
2021-01-29 23:11:02 -0300

    feat: adicionando Topbar e Footer layout base

---
+++ src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-footer class="column items-center  q-gutter-y-md q-py-md">
+    <img
+      alt="Quasar logo"
+      src="~assets/logo.svg"
+      class="logo q-ma-md"
+    >
+    <strong class="text-h6"> Juntos Somos Mais Fidelização S.A</strong>
+    <span> Siga-nos em nossas redes sociais: </span>
+    <section>
+      <q-icon name="facebook" size="md"></q-icon>
+      <q-icon name="instagram" size="md"></q-icon>
+    </section>
+  </q-footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped>
+.logo{
+  width: 10em;
+}
+</style>
---
+++ src/components/TopBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <q-header>
+    <q-toolbar class="justify-between q-py-md">
+      <img
+        alt="Quasar logo"
+        src="~assets/logo.svg"
+        class="logo q-ma-md"
+      >
+
+      <q-input color="teal" rounded  dark filled v-model="text" placeholder="Pesquisar" class="full-width">
+        <template v-slot:prepend>
+          <q-icon name="search"/>
+        </template>
+      </q-input>
+
+      <div class="q-ma-md hideMobyle" id="contatoTopBar">
+        Contato
+      </div>
+
+    </q-toolbar>
+  </q-header>
+</template>
+
+<script>
+export default {
+  name: 'TopBar',
+  data () {
+    return {
+      text: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+.logo {
+  width: 8em;
+}
+</style>
