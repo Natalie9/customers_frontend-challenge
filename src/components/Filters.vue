7cc8afe7e8f9f340de151e7cc484638c396bb940
natalie9 <natalietolentino9@gmail.com>
2021-01-30 11:12:59 -0300

    feat: componente de filtros por regiao dos clientes

---
+++ src/components/Filters.vue
@@ -0,0 +1,47 @@
+<template>
+  <q-page>
+    <strong class="text-h6 q-ma-md"> Filtrar por usuários: </strong>
+    <q-option-group class="q-ma-md"
+      :options="filters"
+      type="checkbox"
+      v-model="group"
+    />
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'Filters',
+  data () {
+    return {
+      filters: [
+        {
+          label: 'Especial',
+          value: 'especial',
+          color: 'grey'
+        },
+        {
+          label: 'Normal',
+          value: 'normal',
+          color: 'grey'
+        },
+        {
+          label: 'Trabalhoso',
+          value: 'trabalhoso',
+          color: 'grey'
+        }
+      ],
+      group: []
+    }
+  },
+  watch: {
+    group () {
+      this.$emit('updateFilters', this.group)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
