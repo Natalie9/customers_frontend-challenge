b7923489399ffea17190fa901aed1f23c0acd1d6
natalie9 <natalietolentino9@gmail.com>
2021-01-29 23:10:11 -0300

    First Commit 🎉

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <TopBar></TopBar>
+
+    <q-page-container>
+      <router-view/>
+    </q-page-container>
+
+    <Footer></Footer>
+  </q-layout>
+</template>
+
+<script>
+import TopBar from 'components/TopBar'
+import Footer from 'components/Footer'
+
+export default {
+  name: 'MainLayout',
+  components: { Footer, TopBar },
+  data () {
+    return {}
+  }
+}
+</script>
