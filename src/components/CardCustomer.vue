8a5484879d0d9511fe4f13a9cbcf75e5f060ab01
natalie9 <natalietolentino9@gmail.com>
2021-02-03 02:43:15 -0300

    feat: ajuste de estilo no cardCustomer

--- src/components/CardCustomer.vue
+++ src/components/CardCustomer.vue
@@ -1,24 +1,23 @@
 <template>
-  <div>
-    <!--    @todo ajustar tamanho padrao para todos-->
-    <div class="card_customer" @click="toPageCustomer">
+  <div class="card" @click="toPageCustomer">
 
-      <img :src="customer.picture.large" class="photo"/>
-
-      <div class="title">
+    <img :src="customer.picture.large" class="card__image"/>
+    <div class="card__content">
+      <div class="card__title">
         <strong> {{ customer.name.fullName }} </strong>
-        <span class="cep">
+
+      </div>
+      <span class="cep">
          {{ customer.cell }}
         </span>
-      </div>
-      <section class="text-center items-baseline infos">
-        <div>
-          {{ cityStateCustomer }}
-        </div>
-      </section>
+      <address class="card__text">
+        {{ cityStateCustomer }}
+      </address>
     </div>
 
+
   </div>
+
 </template>
 
 <script>
@@ -49,44 +48,47 @@ export default {
 }
 </script>
 
-<style scoped>
-.card_customer {
-  display: grid;
-  grid-template-areas:
-  "photo photo"
-  "title title"
-  "infos infos";
-  border-radius: 3px;
-  background: rgba(255,255,255,0.50);
-  box-shadow: 0 1px 2px 1px rgba(0, 0, 0, 0.1);
-  margin: 16px 8px;
-  justify-items: center;
-  height: calc(35vh + 8px);
-  width: 15vw;
-}
-
-.card_customer .photo {
-  grid-area: photo;
-  width: 85%;
-  border-radius: 5px;
-  margin: 4px;
-  border: 1px solid #e7e7e7;
-}
-
-.card_customer .title {
-  grid-area: title;
+<style lang="scss">
+.card {
+  height: 100%;
+  max-height: 300px;
+  max-width: 250px;
+  min-width: 200px;
+  border-radius: 24px;
+  box-shadow: 0 16px 40px 0 rgb(0 0 0 / 10%);
+  background-color: #fff;
+  align-items: center;
   display: flex;
   flex-direction: column;
-  justify-content: left;
-}
+  padding: var(--space-sm) var(--space) var(--space-md);
 
-.card_customer .title .cep {
-  display: flex;
-  align-self: flex-end;
-}
 
-.card_customer .infos {
-  grid-area: infos;
+  &__image {
+    border-radius: 24px;
+    background-color: #ddd;
+    display: block;
+    object-fit: cover;
+    width: 80%;
+    height: 50%;
+  }
+
+  &__title {
+    font-size: 1.2rem;
+    font-weight: 600;
+    color: var(--orange);
+
+    &:not(:last-child) {
+      margin-bottom: var(--space-xs);
+    }
+
+  }
+
+  &__text {
+    font-size: 1rem;
+    line-height: 2.1rem;
+    color: var(--gray-dark);
+  }
+
 }
 
 </style>
